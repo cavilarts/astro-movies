dfc128532ea4494f1614ea394e311c40478e7807
Carlos Avila <cavilart21@gmail.com>
2023-09-14 14:18:37 +0200

    adding like component

---
+++ src/components/Like/LikeButton.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { AiTwotoneHeart, AiOutlineHeart } from "react-icons/ai";
+
+export interface LikeButtonProps {
+  id: number;
+}
+
+export default function LikeButton({ id }: LikeButtonProps) {
+  const [liked, setLiked] = useState<boolean>(false);
+
+  const getLikeFromLocalStorage = () => {
+    const like = localStorage.getItem(`${id}`);
+
+    if (like) {
+      setLiked(true);
+    }
+  };
+
+  function handleLike(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setLiked((liked) => !liked);
+
+    if (liked) {
+      localStorage.removeItem(`${id}`);
+    } else {
+      localStorage.setItem(`${id}`, `${id}`);
+    }
+  }
+
+  useEffect(() => {
+    getLikeFromLocalStorage();
+  }, []);
+
+  return (
+    <button
+      className={`text-5xl ${liked ? "text-red-500" : "text-white"}`}
+      onClick={handleLike}
+    >
+      {liked && <AiTwotoneHeart />}
+      {!liked && <AiOutlineHeart />}
+    </button>
+  );
+}
