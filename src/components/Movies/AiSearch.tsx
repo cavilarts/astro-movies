8818971740d41f8624af80a0d398509bedcdd5b7
Carlos Avila <cavilart21@gmail.com>
2023-10-03 16:03:29 +0200

    adding open AI integration

---
+++ src/components/Movies/AiSearch.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { IoMdSend } from "react-icons/io";
+import Movie from "./Movie";
+import type { MovieResult } from "../../types/default";
+
+export interface AiSearchProps {
+  title: string;
+  description: string;
+}
+
+export default function AiSearch({ title, description }: AiSearchProps) {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  async function onSearch() {
+    setLoading(true);
+
+    try {
+      const response = await fetch("/api/v1/astro-movies", {
+        method: "POST",
+        body: JSON.stringify({ prompt: query }),
+      });
+
+      const data = await response.json();
+
+      setMovies(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  function onEnter(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (event.key === "Enter" && event.shiftKey === false) {
+      onSearch();
+    }
+  }
+
+  function onChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setQuery(event.target.value);
+  }
+
+  return (
+    <section className="w-full h-screen flex flex-col justify-center items-center max-w-7xl mx-auto p-8">
+      <h1 className="text-white font-openSans font-extrabold text-5xl pb-6">
+        {title}
+      </h1>
+      <p className="text-white pb-10">{description}</p>
+      <div className="flex bg-white rounded-lg w-full">
+        <textarea
+          autoComplete="off"
+          name="q"
+          rows={1}
+          onKeyUp={onEnter}
+          onChange={onChange}
+          placeholder="Enter your prompt and find your next movie"
+          className="w-full rounded-full mx-auto text-lg p-4 border-transparent focus:border-transparent focus:ring-0 focus-visible:outline-0 resize-none"
+        />
+        <button
+          className="text-emerald-300 font-bold py-2 px-4 rounded-full text-xl"
+          onClick={onSearch}
+        >
+          <IoMdSend />
+        </button>
+      </div>
+      {loading && <p className="text-white">Loading...</p>}
+      <section className="flex gap-8 max-w-7xl mx-auto flex-wrap justify-start mt-8 pb-8">
+        {movies.map((movie: MovieResult) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </section>
+    </section>
+  );
+}
---
+++ src/pages/api/v1/astro-movies.ts
@@ -0,0 +1,70 @@
+import type { APIContext } from "astro";
+import { OpenAI } from "openai";
+
+export async function POST({ params, request }: APIContext) {
+  const body = await request.json();
+  const prompt = body.prompt;
+
+  console.log("prompt", prompt);
+
+  if (prompt) {
+    const openAI = new OpenAI({
+      apiKey: process.env.PUBLIC_AI_API_KEY,
+    });
+
+    const chat = await openAI.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Could you please recommend me a movie with the topic: ${prompt}? in any genre and any language. please just provide a name or a comma separated list of names whitout spaces and quotes.`,
+          name: "User",
+        },
+      ],
+    });
+    console.log("chat", JSON.stringify(chat.choices));
+    const movies = chat.choices
+      .map((choice) =>
+        choice.message.content?.split(",").map((movie) => {
+          const url = import.meta.env.PUBLIC_MOVIE_DB_API_URL;
+          return `${url}${
+            import.meta.env.PUBLIC_MOVIE_DB_MOVIES_SEARCH_PATH
+          }?query=${movie.trim()}&include_adult=false&page=1`;
+        })
+      )
+      .flatMap((movie) => movie)
+      .filter((movie) => movie);
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.PUBLIC_MOVIE_DB_ACCESS_TOKEN}`,
+      },
+    };
+    const moviesResponses = await Promise.all(
+      movies.map(
+        (movie) =>
+          movie &&
+          fetch(movie, options).then((res) =>
+            res.json().then((json) => json.results[0])
+          )
+      )
+    );
+
+    console.log("moviesResponses", moviesResponses);
+
+    return new Response(JSON.stringify(moviesResponses), {
+      headers: { "content-type": "application/json; charset=UTF-8" },
+      status: 200,
+    });
+  }
+
+  return new Response(
+    JSON.stringify({
+      error: "No prompt provided",
+    }),
+    {
+      headers: { "content-type": "application/json; charset=UTF-8" },
+    }
+  );
+}
